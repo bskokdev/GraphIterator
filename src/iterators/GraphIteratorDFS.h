6b7854a965fab32414fa9a6a56d3f8918d85e2de
Boris Skok <skokcmd@gmail.com>
2023-04-18 10:52:23 +0200

    created dfs iterator definition

--- src/iterators/GraphIteratorDFS.h
+++ src/iterators/GraphIteratorDFS.h
@@ -3,6 +3,7 @@
 
 
 #include <set>
+#include <stack>
 #include "../Graph.h"
 #include "GraphBaseIterator.h"
 
@@ -11,6 +12,58 @@
  */
 class GraphIteratorDFS : public GraphBaseIterator {
 private:
+    Graph& graph;
+    std::set<int> visited;
+    std::stack<std::pair<int, std::vector<int>>> stack;
+public:
+private:
+    /**
+     * @brief resets the iterator to the beginning
+     * @see GraphBaseIterator::reset()
+     */
+    void reset() override;
+
+    /**
+     * @brief checks if the iterator is at the end
+     * @see GraphBaseIterator::isEnd()
+     */
+    bool isEnd() override;
+
+    /**
+     * @brief increments the iterator
+     * @see GraphBaseIterator::next()
+     */
+    GraphBaseIterator &next() override;
+
+    /**
+     * @brief gets the current key of the iterator
+     * @see GraphBaseIterator::currentKey()
+     */
+    int currentKey() override;
+
+    /**
+     * @brief increments the iterator (prefix)
+     * @see GraphBaseIterator::operator++()
+     */
+    GraphBaseIterator &operator++() override;
+
+    /**
+     * @brief increments the iterator (postfix)
+     * @see GraphBaseIterator::operator++(int)
+     */
+    std::shared_ptr<GraphBaseIterator> operator++(int i) override;
+
+    /**
+     * @brief dereferences the iterator
+     * @see GraphBaseIterator::operator*()
+     */
+    int operator*() override;
+
+    /**
+     * @brief checks if the iterator is not equal to another iterator
+     * @see GraphBaseIterator::operator!=()
+     */
+    bool operator!=(const GraphBaseIterator &other) override;
 };
 
 
