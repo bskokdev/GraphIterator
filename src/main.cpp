7d12b64dd0f595ffc76d09c828ef062c0b570d8a
Boris Skok <skokcmd@gmail.com>
2023-05-12 17:02:04 +0200

    fixed args reading

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,15 @@
 #include "../include/Reader.h"
 #include "../include/Application.h"
 
-/**
- * @brief gets the file path from the arguments
- * @param argv input file path
- * @return input file path as string
- */
-std::string getFilePathFromArgs(char **argv) {
-    std::string filePath = argv[1];
-    if (filePath.empty()) {
-        std::cout << "Please provide a file path as an argument" << std::endl;
-        exit(1);
-    }
-    return filePath;
-}
-
 int main(int argc, char *argv[]) {
-    Reader reader(getFilePathFromArgs(argv));
-    Application app(reader);
+    // Check for valid args
+    if(argc < 2) {
+        std::cout << "Please provide a file path as an argument" << std::endl;
+        return 1;
+    }
 
+    Reader reader(argv[1]);
+    Application app(reader);
     app.run();
     return 0;
 }
